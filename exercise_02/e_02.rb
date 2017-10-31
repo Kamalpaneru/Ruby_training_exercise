72f1a078ec1b044bae5f7a3a696a30d1f1247ad8
kamalpaneru <kamalpaneru.15@gmail.com>
2017-10-31 16:48:21 +0545

    Exercise_2

---
+++ exercise_02/e_02.rb
@@ -0,0 +1,6 @@
+# write your code here
+
+#write a method far_to_cel that takes a parameter temp and converts temp in fahrenheit to celsius.
+
+#write a method cel_to_far  that takes a parameter and converts temp in celsius to fahrenheit.
+

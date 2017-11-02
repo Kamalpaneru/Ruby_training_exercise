0fdc20bd68fe0151ec2f9d25df180a511dbae6ae
kamalpaneru <kamalpaneru.15@gmail.com>
2017-11-02 09:41:59 +0545

    Final exercise_2

--- exercise_02/e_02.rb
+++ exercise_02/e_02.rb
@@ -1,14 +1,12 @@
 # Daffy Duckify a users string replacing each 's' with 'th'.
 
-# Ask user to `Enter any string` and store it into a variable.
+# Ask user to `Enter any sentence` and store it into a variable.
 
 # Convert response into lowercase.
 
-#Use include? method to see if the response contains letter 's'.
+#Use `include?` method to see if the response contains letter 's'.
 
-# If the string contains  letter 's' substitute it with 'th' using `gsub!`  method else print `Nothing to replace`.
-
-
+# If the sentence contains  letter 's' substitute it with 'th' using `gsub!`  method else print `Nothing to replace`.
 
 
 
